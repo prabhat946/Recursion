951b7149086d49680213bc2cccac4bb719d6d6e6
prabhat946 <chakravarthula.prabhat.btech2023@sitpune.edu.in>
2024-10-07 12:55:30 +0530

    Reverse the Number.cpp

---
+++ Reverse the Number.cpp
@@ -0,0 +1,28 @@
+//Name:prabhat
+//PRN:23070123040
+//Aim:Implemention of Recursion to find Reverse of given Number
+
+#include <iostream>
+using namespace std;
+
+void rev(int i) {
+    if (i > 0) {
+        cout << (i % 10); 
+        rev(i / 10);      
+    }
+}
+
+int main() {
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+    cout << "Reversed number is: ";
+    rev(num);
+    cout << endl;
+}
+
+
+/*
+Enter a number: 20
+Reversed number is: 02
+*/
