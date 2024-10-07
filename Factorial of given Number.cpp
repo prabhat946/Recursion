75d39d652a5822923c969fc9b8705898c5ef1e9b
prabhat946 <chakravarthula.prabhat.btech2023@sitpune.edu.in>
2024-10-07 12:53:25 +0530

    Factorial of given Number.cpp

---
+++ Factorial of given Number.cpp
@@ -0,0 +1,24 @@
+//Name:prabhat
+//PRN:23070123040
+//Aim:Implemention of Recursion to find Factorial of given Number
+
+#include<iostream>
+using namespace std;
+int factorial(int n) {
+    if (n <= 1)
+        return 1;
+    else 
+        return (n * factorial(n - 1));
+}
+int main() {
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+    cout << "Factorial of " << num << " is " << factorial(num) << endl;
+    return 0;
+}
+
+
+/*Output:
+Enter a number: 5
+Factorial of 5 is 120*/
