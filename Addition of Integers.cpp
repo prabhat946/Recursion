af2bf7968b8f2723ab0abdd1fad977c948ff2af1
prabhat946 <chakravarthula.prabhat.btech2023@sitpune.edu.in>
2024-10-07 12:52:47 +0530

    Addition of Integers.cpp

---
+++ Addition of Integers.cpp
@@ -0,0 +1,27 @@
+//Name:prabhat
+//PRN:23070123040
+//Aim:Implemention of Recursion to find Sum of given Integers
+
+#include<iostream>
+using namespace std;
+
+int sum(int n) {
+    if (n == 0)
+        return 0;
+    else
+        return n + sum(n - 1);
+}
+
+int main() {
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+    cout << "Sum of integers is: " << sum(num) << endl;
+    return 0;
+}
+
+
+/*
+Enter a number: 5
+Sum of integers is: 15
+*/
