b2e6c4bd1dfc2f49cec1379e815c850cf2f96b97
prabhat946 <chakravarthula.prabhat.btech2023@sitpune.edu.in>
2024-10-07 12:54:57 +0530

    Reverse String.cpp

---
+++ Reverse String.cpp
@@ -0,0 +1,27 @@
+//Name:prabhat
+//PRN:23070123040
+//Aim:Implemention of Recursion to Reverse the given String
+
+#include <iostream>
+using namespace std;
+
+void rev(char* str) {
+    if (*str != '\0') {
+        rev(str + 1); 
+        cout << *str; 
+    }
+}
+
+int main() {
+    char str[20];
+    cout << "Enter a string: ";
+    cin >> str;
+    cout << "Reversed string is: ";
+    rev(str);
+    cout << endl;
+}
+
+
+/*Output:
+Enter a string: Hello
+Reversed string is: olleH*/
